d12d504567db05f88d6da7524b929e96e89818a6
solflowfun <254582301+PumpOStrade@users.noreply.github.com>
2025-09-16 19:22:41 -0400

    test setup and fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+"""Shared test fixtures."""
+
+import asyncio
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
